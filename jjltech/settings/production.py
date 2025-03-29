ddaf11a754ea571af45ac4b24aa0f8a6bfdb6c0e
John J Lawal <jophilip79@gmail.com>
2025-03-29 12:45:51 +0100

    Update MEDIA_URL to use the correct AWS S3 region for production

--- jjltech/settings/production.py
+++ jjltech/settings/production.py
@@ -66,4 +66,4 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
 AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com/'
