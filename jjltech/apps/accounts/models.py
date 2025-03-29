72e4092f1a4fd28de0877e8708fbc8ce6d9255a5
John J Lawal <jophilip79@gmail.com>
2025-03-29 16:58:00 +0100

    Refactor profile image handling and add bio, created_at, and updated_at fields

--- jjltech/apps/accounts/models.py
+++ jjltech/apps/accounts/models.py
@@ -10,21 +10,23 @@ class Profile(models.Model):
     country = CountryField(blank_label='(select country)', default='NG')
     headline = models.CharField(max_length=150, blank=True)
     profile_image = models.ImageField(upload_to='profile_pics/', blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
         if self.profile_image:
-
             # Get the file extension
             ext = os.path.splitext(self.profile_image.name)[1]
             # Rename the file to the user's username
             new_filename = f"{self.user.username}{ext}"
             # Save the file with the new name
-            self.profile_image.name = f"profile_pics/{new_filename}"
+            self.profile_image.name = f"{new_filename}"
+
+        if self.profile_image:
+            print(f"Saving file to: {self.profile_image.name}")
 
         super().save(*args, **kwargs)
-    bio = models.TextField(max_length=1000, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
