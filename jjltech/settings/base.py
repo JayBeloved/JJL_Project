ea1a14ad34ccc14f34a65d178e7917e5c8a62d38
John J Lawal <jophilip79@gmail.com>
2025-03-29 18:21:00 +0100

    Update MEDIA_ROOT path and configure AWS S3 settings for production

--- jjltech/settings/base.py
+++ jjltech/settings/base.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR.parent.parent / "media"
 
 # ==============================================================================
 # FIRST-PARTY SETTINGS
