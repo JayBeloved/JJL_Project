58bf10679a86646b3562f15cc9f2f2ac35c417b8
John J Lawal <jophilip79@gmail.com>
2025-03-29 11:20:51 +0100

    Rename profile image files to include the user's username upon saving

--- jjltech/apps/accounts/models.py
+++ jjltech/apps/accounts/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
+import os
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     country = CountryField(blank_label='(select country)', default='NG')
     headline = models.CharField(max_length=150, blank=True)
     profile_image = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.profile_image:
+
+            # Get the file extension
+            ext = os.path.splitext(self.profile_image.name)[1]
+            # Rename the file to the user's username
+            new_filename = f"{self.user.username}{ext}"
+            # Save the file with the new name
+            self.profile_image.name = f"profile_pics/{new_filename}"
+
+        super().save(*args, **kwargs)
     bio = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
